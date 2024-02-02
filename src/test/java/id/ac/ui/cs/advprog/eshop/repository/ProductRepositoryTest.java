c39db1d56592c767e1db36fc37e4b6abd3e99c38
mma <111668501+mmalkindi@users.noreply.github.com>
2024-02-02 17:00:21 +0700

    add unit test for findById

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -62,4 +62,33 @@ class ProductRepositoryTest {
         assertEquals(product2.getProductId(), savedProduct.getProductId());
         assertFalse(productIterator.hasNext());
     }
+
+    @Test
+    void testFindByIdIfEmpty() {
+        assertNull(productRepository.findById("5ba5eee5-99a4-43e2-9be2-26f02557d741"));
+    }
+
+    @Test
+    void testFindByIdIfMoreThanOneProduct() {
+        Product product1 = new Product();
+        product1.setProductId("93e4a6d8-a596-4e9e-bd30-25a80b676740");
+        product1.setProductName("Lumba Lumba Asli Sumatra");
+        product1.setProductQuantity(120);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("8539355a-e19c-4402-86f5-f93ecd5945b2");
+        product2.setProductName("Lumba Lumba Asli Sulawesi");
+        product2.setProductQuantity(80);
+        productRepository.create(product2);
+
+        Product product3 = new Product();
+        product3.setProductId("f77a6d7c-cbd5-44c3-9f06-70064444f8e5");
+        product3.setProductName("Lumba Lumba Asli Papua");
+        product3.setProductQuantity(20);
+        productRepository.create(product3);
+
+        assertNotNull(productRepository.findById("f77a6d7c-cbd5-44c3-9f06-70064444f8e5"));
+        assertNull(productRepository.findById("g77a6d7c-cbd5-44c3-9f06-70064444f8e6"));
+    }
 }
