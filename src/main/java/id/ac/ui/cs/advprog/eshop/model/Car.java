8e847327e8ae7aa8da454d1bc4ec277b34ff587d
mma <111668501+mmalkindi@users.noreply.github.com>
2024-02-16 19:53:20 +0700

    minor space formatting changes

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
+
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.UUID;
 
 @Getter @Setter
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,4 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
